5b55618c661a186238e81e2e7fc29c3e35b312b0
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-02 11:58:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,14 @@ original_array = string.split(" ")
   end
   return_array.join(" ")
 end
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string|
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")
